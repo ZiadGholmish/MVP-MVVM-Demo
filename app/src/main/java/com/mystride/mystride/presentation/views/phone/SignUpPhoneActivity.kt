0aee2d2d2d100ffe31ef31ff65d09a4d6074633a
Ziad Gholmish <ZiadGholmish@gmail.com>
2018-06-30 22:42:37 +0300

    Add the validation to the signup phone

--- app/src/main/java/com/mystride/mystride/presentation/views/phone/SignUpPhoneActivity.kt
+++ app/src/main/java/com/mystride/mystride/presentation/views/phone/SignUpPhoneActivity.kt
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import com.github.vacxe.phonemask.PhoneMaskManager
+import com.jakewharton.rxbinding2.widget.RxTextView
 import com.mystride.mystride.R
 import com.mystride.mystride.presentation.views.country.CountriesCodesActivity
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_sign_up_phone.*
 
 class SignUpPhoneActivity : AppCompatActivity() {
@@ -18,11 +20,13 @@ class SignUpPhoneActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setPhoneMask()
         openCountriesCodeScreen()
+
+        addPhoneNumberObservable()
     }
 
     private fun setPhoneMask() {
         PhoneMaskManager()
-                .withMask("(###) ###-###-###")
+                .withMask("(###) ###-####")
                 .withValueListener { }
                 .bindTo(phone_number)
     }
@@ -33,6 +37,23 @@ class SignUpPhoneActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun addPhoneNumberObservable(): Observable<Boolean> {
+        phone_number_layout.error = getString(R.string.please_enter_your_mobile_phone_number)
+        val phoneNumberObservable = RxTextView
+                .textChanges(phone_number)
+                .map { inputText -> inputText.length == 14 }
+                .distinctUntilChanged()
+        phoneNumberObservable.subscribe { isValidPhone ->
+            phone_number_layout.isErrorEnabled = !isValidPhone
+            btn_continue.isEnabled = isValidPhone
+            if (!isValidPhone) {
+                phone_number_layout.error = getString(R.string.please_enter_your_mobile_phone_number)
+            }
+        }
+        return phoneNumberObservable
+    }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
