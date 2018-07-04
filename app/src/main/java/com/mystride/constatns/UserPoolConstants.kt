af7f026418dbb1e2a3cbfb5c6ce516f642bad5d6
Ziad Gholmish <ziadgholmish@gmail.com>
2018-07-04 22:36:58 +0300

    Push the latest code

--- app/src/main/java/com/mystride/constatns/UserPoolConstants.kt
+++ app/src/main/java/com/mystride/constatns/UserPoolConstants.kt
@@ -35,7 +35,9 @@ object UserPoolConstants {
     const val COGINTO_USER_ATTRIBUTES_PHONE_NUMBER = "phone_number"
     const val COGINTO_USER_ATTRIBUTES_GIVEN_NAME = "given_name"
     const val COGINTO_USER_ATTRIBUTES_FAMILY_NAME = "family_name"
+    const val COGINTO_USER_ATTRIBUTES_PREFERRED_USERNAME = "preferred_username"
     const val COGINTO_USER_ATTRIBUTES_LOCALE = "locale"
+
 
     //default Locale
     const val LOCALE_US = "US"
